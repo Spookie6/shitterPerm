e3898f9b895d5d79a7a3687aa79f23c6582d1afe
GitHub Action <action@github.com>
2024-03-15 08:45:03 +0000

    Initialize template repository proper
    Set package name to com.github.spookie6.shitterperm
    Set project name to shitterPerm
    Set modid to shitterperm

--- src/main/java/com/github/spookie6/shitterperm/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/spookie6/shitterperm/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.spookie6.shitterperm.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
